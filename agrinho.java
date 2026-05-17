ad39b269fc719f5cc0d55072537736110cffe5c8
rremi1 <miersvarenatha@gmail.com>
2026-05-17 14:50:02 -0300

    .

--- agrinho.java
+++ agrinho.java
@@ -1,7 +1,29 @@
-function irParaSecao(id){
+<!-- CARD MEIO AMBIENTE -->
+<div class="card">
 
-    document.getElementById(id).scrollIntoView({
-        behavior: "smooth"
-    });
+    <img src="floresta.jpg" alt="Meio Ambiente">
 
-}
+    <button onclick="irParaSecao('meioAmbiente')">
+        Meio Ambiente
+    </button>
+
+    <p class="clique">
+        (clique para saber mais)
+    </p>
+
+</div>
+
+<!-- CARD PRODUÇÃO -->
+<div class="card">
+
+    <img src="producao.jpg" alt="Produção">
+
+    <button onclick="irParaSecao('producao')">
+        Produção
+    </button>
+
+    <p class="clique">
+        (clique para saber mais)
+    </p>
+
+</div>
