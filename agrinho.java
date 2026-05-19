67a9285c67e174908a6654e79f087cf5810637bd
rremi1 <miersvarenatha@gmail.com>
2026-05-18 23:06:27 -0300

    .

--- agrinho.java
+++ agrinho.java
@@ -47,3 +47,13 @@ imagens.forEach((imagem) => {
     });
 
 });
+
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+
+    if(window.scrollY > 50){
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
