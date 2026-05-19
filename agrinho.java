32478bf65948bd228dfe3210e475018117289c44
rremi1 <miersvarenatha@gmail.com>
2026-05-18 23:09:52 -0300

    .

--- agrinho.java
+++ agrinho.java
@@ -47,13 +47,3 @@ imagens.forEach((imagem) => {
     });
 
 });
-
-window.addEventListener("scroll", () => {
-    const header = document.querySelector("header");
-
-    if(window.scrollY > 50){
-        header.classList.add("scrolled");
-    } else {
-        header.classList.remove("scrolled");
-    }
-});
