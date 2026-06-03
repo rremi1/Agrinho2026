8354a51f40b21226d79c81f97d731f2ad7f3f837
rremi1 <miersvarenatha@gmail.com>
2026-06-03 09:38:06 -0300

    .

--- agrinho.java
+++ agrinho.java
@@ -26,18 +26,11 @@ function contar(id, valorFinal, velocidade){
     }, velocidade);
 }
 
-// inicia os contadores
-contar("arvores", 1000, 20);
-contar("agua", 5000, 10);
-contar("alimentos", 2000, 15);
-async function atualizarDados(){
+const socket = new WebSocket("wss://seuservidor.com");
 
-    const resposta = await fetch("https://api.exemplo.com/impacto");
-    const dados = await resposta.json();
+socket.onmessage = (event) => {
+
+    const dados = JSON.parse(event.data);
 
     document.getElementById("arvores").innerText = dados.arvores;
-    document.getElementById("agua").innerText = dados.agua;
-    document.getElementById("alimentos").innerText = dados.alimentos;
-}
-
-setInterval(atualizarDados, 5000);
+};
