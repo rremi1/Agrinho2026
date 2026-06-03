090501a3b4e862cf41cc849e0cf2562f6dfdc3aa
rremi1 <miersvarenatha@gmail.com>
2026-06-03 09:23:21 -0300

    .

--- agrinho.java
+++ agrinho.java
@@ -5,21 +5,3 @@ document.getElementById("btn").addEventListener("click", () => {
         behavior:"smooth"
     });
 });
-
-// CONTADOR
-
-let numero = document.getElementById("numero");
-
-let valor = 0;
-
-setInterval(() => {
-
-    valor += 10;
-
-    numero.innerText = valor;
-
-    if(valor >= 1000){
-        valor = 1000;
-    }
-
-}, 20);
