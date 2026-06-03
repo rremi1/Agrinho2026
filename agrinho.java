bd3879054c5e315337c167e7f92f3357270851f8
rremi1 <miersvarenatha@gmail.com>
2026-06-03 09:39:28 -0300

    ,

--- agrinho.java
+++ agrinho.java
@@ -5,27 +5,6 @@ document.getElementById("btn").addEventListener("click", () => {
         behavior:"smooth"
     });
 });
-function contar(id, valorFinal, velocidade){
-
-    const el = document.getElementById(id);
-
-    if(!el) return;
-
-    let valor = 0;
-
-    const intervalo = setInterval(() => {
-
-        valor += 10;
-        el.innerText = valor;
-
-        if(valor >= valorFinal){
-            el.innerText = valorFinal;
-            clearInterval(intervalo);
-        }
-
-    }, velocidade);
-}
-
 const socket = new WebSocket("wss://seuservidor.com");
 
 socket.onmessage = (event) => {
