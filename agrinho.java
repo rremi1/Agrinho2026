72abab44400a19b1fda0405c86866ce768edee92
rremi1 <miersvarenatha@gmail.com>
2026-06-03 09:35:01 -0300

    .

--- agrinho.java
+++ agrinho.java
@@ -5,3 +5,28 @@ document.getElementById("btn").addEventListener("click", () => {
         behavior:"smooth"
     });
 });
+function contar(id, valorFinal, velocidade){
+
+    const el = document.getElementById(id);
+
+    if(!el) return;
+
+    let valor = 0;
+
+    const intervalo = setInterval(() => {
+
+        valor += 10;
+        el.innerText = valor;
+
+        if(valor >= valorFinal){
+            el.innerText = valorFinal;
+            clearInterval(intervalo);
+        }
+
+    }, velocidade);
+}
+
+// inicia os contadores
+contar("arvores", 1000, 20);
+contar("agua", 5000, 10);
+contar("alimentos", 2000, 15);
