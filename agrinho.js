3a21b1954ac24e34200ac71ef3c00fea33a8cf1e
rremi1 <miersvarenatha@gmail.com>
2026-06-09 15:27:35 -0300

    .

--- agrinho.js
+++ agrinho.js
@@ -10,3 +10,20 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+function animarNumero(id, final, velocidade) {
+    let el = document.getElementById(id);
+    let atual = 0;
+
+    let intervalo = setInterval(() => {
+        atual += Math.ceil(final / 50);
+        if (atual >= final) {
+            atual = final;
+            clearInterval(intervalo);
+        }
+        el.innerText = atual;
+    }, velocidade);
+}
+
+animarNumero("arvores", 1250, 30);
+animarNumero("agua", 5400, 30);
+animarNumero("alimentos", 820);
