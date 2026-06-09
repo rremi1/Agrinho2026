19667c41372839448ff7efa61aa341a5af4db5e9
rremi1 <miersvarenatha@gmail.com>
2026-06-09 14:43:21 -0300

    .

--- agrinho.java
+++ agrinho.java
@@ -11,3 +11,13 @@ setInterval(() => {
     arvores += Math.floor(Math.random() * 20);
     document.getElementById("arvores").innerText = arvores;
 }, 1000);
+
+<button
+  onClick={() => {
+    document.getElementById("temas")?.scrollIntoView({
+      behavior: "smooth",
+    });
+  }}
+>
+  Explorar
+</button>
