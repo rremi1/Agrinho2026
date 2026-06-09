c5a0f8c97b9adc3b4d84fbaead77537dd7cb3848
rremi1 <miersvarenatha@gmail.com>
2026-06-09 15:15:15 -0300

    ;

---
+++ agrinho.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const btn = document.getElementById("btn");
+
+    btn.addEventListener("click", () => {
+        alert("Funcionou!");
+    });
+
+});
